d7a6ccd8f80487a57f9d41333b441089f4944a25
KholisEnka22 <enka.run2206@gmail.com>
2020-12-05 11:02:58 +0700

    Funcotion

---
+++ funcotion/CallFuncotion.java
@@ -0,0 +1,14 @@
+package funcotion;
+
+public class CallFuncotion {
+
+    public static void main(String[] args) {
+        // memanggil fungsi 
+        cobaFungsi();
+    }
+ 
+    public static void cobaFungsi() {
+        System.out.println("Ini merupakan bagian dari fungsi");
+    }
+    
+}
